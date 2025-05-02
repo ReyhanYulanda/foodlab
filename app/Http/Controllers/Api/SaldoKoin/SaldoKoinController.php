0000c9243ba232924e49657dad6eff46efda0723
ReyhanYulanda <arimcoding@gmail.com>
2025-05-02 16:47:34 +0700

    perubahan timezone di model transaksisaldokoin

--- app/Http/Controllers/Api/SaldoKoin/SaldoKoinController.php
+++ app/Http/Controllers/Api/SaldoKoin/SaldoKoinController.php
@@ -20,65 +20,6 @@ class SaldoKoinController extends Controller
         ]);
     }
 
-    // public function tambahSaldo(Request $request)
-    // {
-    //     $request->validate([
-    //         'jumlah' => 'required|integer|min:0',
-    //         'deskripsi' => 'nullable|string|max:255'
-    //     ]);
-
-    //     $saldo = SaldoKoin::firstOrCreate(['user_id' => Auth::id()]);
-    //     $saldo->jumlah += $request->jumlah;
-    //     $saldo->save();
-
-    //     TransaksiSaldoKoin::create([
-    //         'user_id' => Auth::id(),
-    //         'jumlah' => $request->jumlah,
-    //         'tipe' => 'masuk',
-    //         'deskripsi' => $request->deskripsi ?? 'Penambahan Saldo Koin'
-    //     ]);
-
-    //     return response()->json([
-    //         'success' => true,
-    //         'message' => 'Saldo Koin berhasil ditambahkan',
-    //         'saldo_koin' => $saldo->jumlah
-    //     ]);
-    // }
-
-    // public function kurangiSaldo(Request $request)
-    // {
-    //     $request->validate([
-    //         'jumlah' => 'required|integer|min:0',
-    //         'deskripsi' => 'nullable|string|max:255'
-    //     ]);
-
-    //     $saldo = SaldoKoin::where('user_id', Auth::id())->first();
-
-    //     if (!$saldo || $saldo->jumlah < $request->jumlah) {
-    //         return response()->json([
-    //             'success' => false,
-    //             'message' => 'Saldo Koin tidak cukup'
-    //         ], 400);
-    //     }
-
-    //     $saldo->jumlah -= $request->jumlah;
-    //     $saldo->save();
-
-    //     // Simpan transaksi
-    //     TransaksiSaldoKoin::create([
-    //         'user_id' => Auth::id(),
-    //         'jumlah' => $request->jumlah,
-    //         'tipe' => 'keluar',
-    //         'deskripsi' => $request->deskripsi ?? 'Pengurangan Saldo Koin'
-    //     ]);
-
-    //     return response()->json([
-    //         'success' => true,
-    //         'message' => 'Saldo Koin berhasil dikurangi',
-    //         'saldo_koin' => $saldo->jumlah
-    //     ]);
-    // }
-
     public function riwayatTransaksi()
     {
         $transaksi = TransaksiSaldoKoin::where('user_id', Auth::id())
--- app/Models/TransaksiSaldoKoin.php
+++ app/Models/TransaksiSaldoKoin.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
 
 class TransaksiSaldoKoin extends Model
 {
@@ -17,5 +18,14 @@ class TransaksiSaldoKoin extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timezone('Asia/Jakarta')->toDateTimeString();
+    }
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timezone('Asia/Jakarta')->toDateTimeString();
     }
 }
