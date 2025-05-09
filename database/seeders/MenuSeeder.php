ae04ed687139b58a9d9708ac55cde51310a5556b
ReyhanYulanda <arimcoding@gmail.com>
2025-05-09 21:54:19 +0700

    penyesuaian seeder

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -10,21 +10,9 @@ use Illuminate\Database\Seeder;
 class MenuSeeder extends Seeder
 {
     use HasMenuPermission;
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
-        // $mm = Menu::firstOrCreate(['url' => 'konfigurasi'],[
-        //     'name' => 'konfigurasi', 'url' => 'konfigurasi', 'category' => 'MASTER DATA',
-        // ]);
-        // $this->attachMenuPermission($mm, ['read'], ['admin']);
-
-        // $sm = $mm->subMenus()->create(['name' => 'menu', 'url' => $mm->url.'/menu', 'category' => $mm->category]);
-        // $this->attachMenuPermission($sm, null, ['admin']);
-
         $mm = Menu::firstOrCreate(['url' => '/role'],[
             'nama' => 'role', 'url' => '/role', 'kategori' => 'KONFIGURASI',
         ]);
@@ -39,30 +27,5 @@ class MenuSeeder extends Seeder
             'nama' => 'menu', 'url' => '/menu', 'kategori' => 'KONFIGURASI',
         ]);
         $this->attachMenuPermission($mm, null, ['admin']);
-
-        // $mm = Menu::firstOrCreate(['url' => '/makanan'],[
-        //     'name' => 'Makanan', 'url' => '/makanan', 'category' => 'USER',
-        // ]);
-        // $this->attachMenuPermission($mm, null, ['tenant']);
-
-        // $mm = Menu::firstOrCreate(['url' => '/katalog'],[
-        //     'name' => 'katalog', 'url' => '/katalog', 'category' => 'KATALOG',
-        // ]);
-        // $this->attachMenuPermission($mm, ['read'], ['user']);
-
-        // $sm = $mm->subMenus()->create(['name' => 'pesan', 'url' => $mm->url.'/pesan', 'category' => $mm->category]);
-        // $this->attachMenuPermission($sm, ['read'], ['user']);
-
-        // $ssm = $sm->subMenus()->create(['name' => 'antar', 'url' => $sm->url.'/antar', 'category' => $sm->category]);
-        // $this->attachMenuPermission($ssm, ['read'], ['user']);
-
-        // $ssm = $sm->subMenus()->create(['name' => 'ambil', 'url' => $sm->url.'/ambil', 'category' => $sm->category]);
-        // $this->attachMenuPermission($ssm, ['read'], ['user']);
-
-        // $ssm = $sm->subMenus()->create(['name' => 'transfer', 'url' => $sm->url.'/transfer', 'category' => $sm->category]);
-        // $this->attachMenuPermission($ssm, ['read'], ['user']);
-
-        // $ssm = $sm->subMenus()->create(['name' => 'cod', 'url' => $sm->url.'/cod', 'category' => $sm->category]);
-        // $this->attachMenuPermission($ssm, ['read'], ['user']);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,20 +7,15 @@ use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        $user = ['admin', 'user', 'tenant', 'kdh'];
+        $user = ['admin'];
 
         foreach($user as $value){
             User::create([
                 'name' =>  $value,
                 'email' => $value.'@gmail.com',
-                'password'=> bcrypt('i12345678'),
+                'password'=> bcrypt('As3!xL9@uQ1%vR8#'),
             ])->assignRole($value);
         }
     }
