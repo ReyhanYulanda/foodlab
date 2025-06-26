50e3bba9d86243838320c5919a6b07671627f7b2
ReyhanYulanda <arimcoding@gmail.com>
2025-06-26 17:12:54 +0700

    percobaan

--- app/Http/Controllers/Transaksi/TransaksiController.php
+++ app/Http/Controllers/Transaksi/TransaksiController.php
@@ -161,7 +161,7 @@ class TransaksiController extends Controller
     public function store(Request $request, Firebases $firebases)
     {
         $user = $request->user();
-        $permission = $user->can('create order');
+        $permission = $user->can('create order_user');
 
         if (!$permission) {
             return response()->json([
