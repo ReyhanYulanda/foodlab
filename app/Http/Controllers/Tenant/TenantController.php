e158715320e5c418461f0220041db6f7a9aceb5e
ReyhanYulanda <arimcoding@gmail.com>
2025-06-26 16:48:29 +0700

    percobaan

--- app/Http/Controllers/Tenant/TenantController.php
+++ app/Http/Controllers/Tenant/TenantController.php
@@ -6,26 +6,34 @@ use App\Http\Controllers\Controller;
 use App\Models\Tenants;
 use App\Response\ResponseApi;
 use Illuminate\Http\Request;
+use App\Services\Firebases;
 
 class TenantController extends Controller
 {
-    public function getAll(Request $request){
-        $user = $request->user()->can('read beranda');
+    public function getAll(Request $request, Firebases $firebases){
+        $user = $request->user();
 
-        if(!$user){
+        if (!$user->can('read beranda')) {
             return response()->json([
                 'status' => 'failed',
                 'message' => 'tidak memiliki akses',
             ], 403);
         }
 
-        $tenants = Tenants::with(['listMenu', 'pemilik'])
-            ->where('user_id', '!=', $request->user()->id)
-            ->get()
-            ->filter(function ($tenant) {
-                return $tenant->pemilik;
-            })
-            ->values();
+        if ($user && $user->fcm_token) {
+            $firebases->withData([
+                'title' => 'Selamat Datang!',
+                'body' => 'Terima kasih telah membuka aplikasi kami 😊',
+            ])->sendMessages($user->fcm_token);
+        }
+
+        // $tenants = Tenants::with(['listMenu', 'pemilik'])
+        //     ->where('user_id', '!=', $request->user()->id)
+        //     ->get()
+        //     ->filter(function ($tenant) {
+        //         return $tenant->pemilik;
+        //     })
+        //     ->values();
 
         return ResponseApi::success(compact('tenants'), 'berhasil mendapatkan data');
     }
