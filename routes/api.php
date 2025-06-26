773ee23aeeb659780e12c0c9eb0bc0c2c947e791
ReyhanYulanda <arimcoding@gmail.com>
2025-06-26 15:43:02 +0700

    menyalakan kembali api /tenants

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::middleware('auth:sanctum')->group(function () {
     // USER 
     Route::get('/katalog/tenants', [TenantController::class, 'getAll']);
     Route::get('/katalog/tenants/{TenantId}', [TenantController::class, 'getSpecificTenant']);
-    // Route::get('/tenants', [TenantController::class, 'getAll']);
+    Route::get('/tenants', [TenantController::class, 'getAll']);
     Route::get('/tenants/{TenantId}', [TenantController::class, 'getSpecificTenant']);
     Route::get('/order/user', [TransaksiController::class, 'orderUser']);
     Route::post('/order', [TransaksiController::class, 'store']);
