f07b01859e6f6805e330d0a3cb4c50358cb87eaf
Dukhaan <dukhaank@gmail.com>
2025-06-30 17:48:14 +0700

    add : email verification still not fixed

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('menu/{id}', [KelolaTenantController::class, 'updateMenuWeb']);
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum', 'verified')->group(function () {
     Route::get('/auth', [UserController::class, 'index']);
     Route::post('/update-user', [UserController::class, 'update']);
     // USER 
