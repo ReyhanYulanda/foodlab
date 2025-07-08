b96367af3ce00b0dba23fb15d5636fb8cde2b9a8
Dukhaan <dukhaank@gmail.com>
2025-07-08 21:13:42 +0700

    change: email request

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -28,7 +28,7 @@
                 <!-- Email Address -->
                 <div>
                     <x-label for="email" :value="__('Email')" />
-                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', request()->email)" required autofocus />
                 </div>
 
                 <!-- Password -->
