2dfb64eb1b9a1d064e780ce4e88bf6c72b59c373
Dukhaan <dukhaank@gmail.com>
2025-07-24 13:17:06 +0700

    fix: request logger on middleware route

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('menu/{id}', [KelolaTenantController::class, 'updateMenuWeb']);
 
-Route::middleware('auth:sanctum', 'verified', 'request.logger')->group(function () {
+Route::middleware('auth:sanctum', 'verified')->group(function () {
     Route::post('/tenant/menucoba/{id}', [KelolaTenantController::class, 'updateMenu']);
 
     Route::get('/auth', [UserController::class, 'index']);
