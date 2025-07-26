a4b8e63f66041b27dd7ae63f5c815827926ad9f8
Dukhaan <dukhaank@gmail.com>
2025-07-26 21:48:16 +0700

    change: reset manual offline now be dynamic on pengaturan

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,20 +4,22 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Pengaturan;
 
 class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
     {
+        $reset_manual = Pengaturan::where('nama', 'reset_manual')->first();
         $schedule->command('order:autocancel')->everyMinute();
         $schedule->command('tenant:update-status')->everyMinute();
         $schedule->command('tenant:tutup')->everyMinute();
-        $schedule->command('tenant:reset-manual-offline')->dailyAt('07:59');
+        $schedule->command('tenant:reset-manual-offline')->dailyAt($reset_manual->nilai ?? '00:00');
     }
 
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
