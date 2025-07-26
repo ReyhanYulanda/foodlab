9afa88188a8c82bffdf83bda1d1721f6ab487e64
Dukhaan <dukhaank@gmail.com>
2025-07-27 00:55:16 +0700

    fix: turn on sendEmailVerif when register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -43,7 +43,7 @@ class AuthController extends Controller
                 'password' => Hash::make($request->password),
                 // 'phone' => $request->phone,
             ]);
-            // $newUser->sendEmailVerificationNotification();
+            $newUser->sendEmailVerificationNotification();
             // Token Management
             $token = $newUser->createToken('secret')->plainTextToken;
 
