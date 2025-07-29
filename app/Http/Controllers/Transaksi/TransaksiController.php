694c39a014a3fd4e8a956d0875e285cb56789408
Dukhaan <dukhaank@gmail.com>
2025-07-29 17:54:55 +0700

    add: add debug to pushToUbisma payload

--- app/Http/Controllers/Transaksi/TransaksiController.php
+++ app/Http/Controllers/Transaksi/TransaksiController.php
@@ -572,10 +572,9 @@ class TransaksiController extends Controller
         // ✅ Kirim dengan format JSON dan header yang benar
         $response = Http::withHeaders([
             'x-api-key' => 'PENS-wQlLZ8M8ruQMeGnoihbeeeXnlOktHZqURaGSV3j1y8YcT3KuW0rcC',
+            'Content-Type' => 'application/json',
             'Accept' => 'application/json',
-            'User-Agent' => 'curl/7.79.1'
-        ])->withBody(json_encode($payload), 'application/json')
-            ->post('https://mis.pens.ac.id/API_PENS/index.php?path=v1/execute_foodlab');
+        ])->post('https://mis.pens.ac.id/API_PENS/index.php?path=v1/execute_foodlab', $payload);
 
         return response()->json([
             'status' => $response->json('status'),
