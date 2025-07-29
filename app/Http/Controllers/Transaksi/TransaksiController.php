d288a132fac302af3970492ccfc6c031bf558888
Dukhaan <dukhaank@gmail.com>
2025-07-29 17:30:25 +0700

    add: add debug to pushToUbisma payload

--- app/Http/Controllers/Transaksi/TransaksiController.php
+++ app/Http/Controllers/Transaksi/TransaksiController.php
@@ -548,6 +548,7 @@ class TransaksiController extends Controller
     public function pushToUbisma(Request $request)
     {
         $data = $request->input('data.0');
+
         $validator = Validator::make($data, [
             'request_id_' => 'required|integer|digits_between:1,10',
             'nama_' => 'required|string|max:100',
@@ -563,7 +564,6 @@ class TransaksiController extends Controller
             ], 422);
         }
 
-        // Payload sesuai spesifikasi API
         $payload = [
             'procedure' => 'pfoodlab_topup',
             'data' => [$data]
@@ -572,13 +572,17 @@ class TransaksiController extends Controller
         // Kirim ke API eksternal
         $response = Http::withHeaders([
             'x-api-key' => 'PENS-wQlLZ8M8ruQMeGnoihbeeeXnlOktHZqURaGSV3j1y8YcT3KuW0rcC',
-        ])->get('https://mis.pens.ac.id/API_PENS/index.php?path=v1/execute_foodlab', $payload);
+        ])->post('https://mis.pens.ac.id/API_PENS/index.php?path=v1/execute_foodlab', $payload);
 
-        // Kembalikan respons dari server eksternal
         return response()->json([
             'status' => $response->json('status'),
             'code' => $response->json('code'),
             'data' => $response->json('data'),
+            'debug' => [
+                'payload_sent' => $payload,
+                'raw_response' => $response->json(),
+                'http_status' => $response->status(),
+            ]
         ], $response->status());
     }
 }
