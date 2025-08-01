9d9ea1c856f63f39cf389c296c3970f259ff9691
Dukhaan <dukhaank@gmail.com>
2025-08-01 18:51:19 +0700

    add: driver offline at 00:00 automatic

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,10 +11,12 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $reset_manual = Pengaturan::where('nama', 'reset_manual')->first();
+        $jam_tutup_driver = Pengaturan::where('nama', 'jam_tutup_driver')->first();
         $schedule->command('order:autocancel')->everyMinute();
         $schedule->command('tenant:update-status')->everyMinute();
         $schedule->command('tenant:tutup')->everyMinute();
         $schedule->command('tenant:reset-manual-offline')->dailyAt($reset_manual->nilai ?? '00:00');
+        $schedule->command('driver:tutup')->dailyAt($jam_tutup_driver->nilai ?? '00:00');
     }
 
     protected function commands()
