ecb1bfb58e663d46bc349d53796a167274bde341
Dukhaan <dukhaank@gmail.com>
2025-08-03 12:48:35 +0700

    add: handler prevention to TenantOrderService

--- app/Services/Kelola/TenantOrderService.php
+++ app/Services/Kelola/TenantOrderService.php
@@ -64,6 +64,30 @@ class TenantOrderService
             'status' => 'required|in:pesanan_ditolak,pesanan_diproses,siap_diantar,siap_diambil,diantar,selesai'
         ]);
 
+        if ($transaksi->status === 'pesanan_diproses' && $request->status === 'pesanan_diproses') {
+            return ResponseApi::error('Pesanan sudah dalam proses sebelumnya.', 403);
+        }
+
+        if ($transaksi->status === 'siap_diantar' && $request->status === 'siap_diantar') {
+            return ResponseApi::error('Pesanan sudah siap diantar sebelumnya.', 403);
+        }
+
+        if ($transaksi->status === 'siap_diambil' && $request->status === 'siap_diambil') {
+            return ResponseApi::error('Pesanan sudah siap diambil sebelumnya.', 403);
+        }
+
+        if ($transaksi->status === 'diantar' && $request->status === 'diantar') {
+            return ResponseApi::error('Pesanan sudah dalam proses pengantaran sebelumnya.', 403);
+        }
+
+        if ($transaksi->status === 'selesai' && $request->status === 'selesai') {
+            return ResponseApi::error('Pesanan sudah selesai sebelumnya.', 403);
+        }
+
+        if ($transaksi->status === 'pesanan_ditolak' && $request->status === 'pesanan_ditolak') {
+            return ResponseApi::error('Pesanan sudah ditolak sebelumnya.', 403);
+        }
+
         if ($validation) {
             return $validation;
         }
