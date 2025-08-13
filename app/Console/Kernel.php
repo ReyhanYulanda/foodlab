ce416355646a2bbcc55cc312a660b059be8dd07b
Dukhaan <dukhaank@gmail.com>
2025-08-13 13:56:25 +0700

    add: sendPesananMasukNotification tenant every minutes pesanan_masuk

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('driver:tutup')->dailyAt($jam_tutup_driver->nilai ?? '00:00');
         $schedule->command('chat:delete-finished')->dailyAt($jam_clear_chat->nilai ?? '00:00');
         $schedule->command('notifikasi:siap-diantar')->everyMinute();
+        $schedule->command('notifikasi:pesanan-masuk')->everyMinute();
         $schedule->command('transaksi:auto-complete-siap-diambil')->dailyAt($jam_siap_diambil_done_otomatis->nilai ?? '02:00');
     }
 
