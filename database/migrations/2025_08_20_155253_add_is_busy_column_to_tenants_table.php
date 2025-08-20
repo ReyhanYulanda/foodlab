1ce775b83d1c83f3c1bf3db3d570274a6436fc2e
Dukhaan <dukhaank@gmail.com>
2025-08-20 16:00:39 +0700

    add: column is_busy to table tenant and command when 2x refund got timeouted

--- database/migrations/2025_08_20_155253_add_is_busy_column_to_tenants_table.php
+++ database/migrations/2025_08_20_155253_add_is_busy_column_to_tenants_table.php
@@ -14,7 +14,7 @@ class AddIsBusyColumnToTenantsTable extends Migration
     public function up()
     {
         Schema::table('tenants', function (Blueprint $table) {
-            $table->timestamp('is_busy')->nullable()->change();
+            $table->dateTime('is_busy')->nullable()->change();
         });
     }
 
@@ -26,7 +26,7 @@ class AddIsBusyColumnToTenantsTable extends Migration
     public function down()
     {
         Schema::table('tenants', function (Blueprint $table) {
-            $table->timestamp('is_busy')->nullable(false)->change();
+            $table->dateTime('is_busy')->nullable(false)->change();
         });
     }
 }
