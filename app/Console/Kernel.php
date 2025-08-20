c7fb4d67dced1a22a9ee24e2720266bf54b1fc05
Dukhaan <dukhaank@gmail.com>
2025-08-20 16:30:19 +0700

    add: column is_busy to table tenant and command when 2x refund got timeouted

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('notifikasi:siap-diantar')->everyMinute();
         $schedule->command('notifikasi:pesanan-masuk')->everyMinute();
         $schedule->command('transaksi:auto-complete-siap-diambil')->dailyAt($jam_siap_diambil_done_otomatis->nilai ?? '02:00');
-        $schedule->command('tenants:reset-busy')->everyMinute();
+        // $schedule->command('tenants:reset-busy')->everyMinute();
         $schedule->command('tenants:check-refund')->everyMinute();
     }
 
