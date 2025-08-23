e0f3c6de4631854e615e6a5a592772d799bf1427
Dukhaan <dukhaank@gmail.com>
2025-08-23 23:50:56 +0700

    fix: transaksi tenant filter date started at changed from 18:00 to 06:00

--- resources/views/pages/transaksi/tenant/index.blade.php
+++ resources/views/pages/transaksi/tenant/index.blade.php
@@ -16,8 +16,8 @@
                     @endif
 
                     <div class="alert alert-info">
-                        <strong>Informasi:</strong> Perhitungan transaksi harian dimulai dari pukul <strong>18:00</strong> hari sebelumnya hingga <strong>17:59</strong> pada hari yang dipilih.
-                        <br>Contoh: Jika Anda memilih tanggal <strong>17 Juni</strong>, maka data yang ditampilkan berasal dari <strong>16 Juni pukul 18:00</strong> sampai <strong>17 Juni pukul 17:59</strong>.
+                        <strong>Informasi:</strong> Perhitungan transaksi harian dimulai dari pukul <strong>06:00</strong> hari sebelumnya hingga <strong>05:59</strong> pada hari yang dipilih.
+                        <br>Contoh: Jika Anda memilih tanggal <strong>17 Juni</strong>, maka data yang ditampilkan berasal dari <strong>16 Juni pukul 06:00</strong> sampai <strong>17 Juni pukul 05:59</strong>.
                     </div>
 
                     <form method="GET" action="{{ route('transaksi.tenant') }}" class="mb-3">
