b811fba6ded646abde7b9b0e40e07c9a1a714d0d
Dukhaan <dukhaank@gmail.com>
2025-09-16 13:52:15 +0700

    fix: error message in transaksi controller while tenant tutup into array

--- app/Http/Controllers/Transaksi/TransaksiController.php
+++ app/Http/Controllers/Transaksi/TransaksiController.php
@@ -258,7 +258,7 @@ class TransaksiController extends Controller
         if ($tenant->isOnline == 0) {
             return response()->json([
                 'status' => 'failed',
-                'message' => 'Toko sedang tutup'
+                'message' => ['Toko sedang tutup']
             ], 400);
         }
 
@@ -270,7 +270,7 @@ class TransaksiController extends Controller
         if ($request->isAntar && $jumlahDriver == 0) {
             return response()->json([
                 'status' => 'failed',
-                'message' => 'Tidak ada driver online saat ini. Silakan coba lagi nanti.'
+                'message' => ['Tidak ada driver online saat ini. Silakan coba lagi nanti.']
             ], 400);
         }
 
