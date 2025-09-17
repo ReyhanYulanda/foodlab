6a0155afefcdd8cc7b0bee8d764431d7076873a5
Dukhaan <dukhaank@gmail.com>
2025-09-17 20:13:49 +0700

    add: change to doughnut chart

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,23 +6,10 @@
     <div class="main-content">
         <div class="title">Dashboard</div>
         <div class="content-wrapper">
-            {{-- ROW ATAS --}}
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Statistik Bulanan</h4>
-                        </div>
-                        <div class="card-body">
-                            <canvas id="chartBulanan"></canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            {{-- ROW BAWAH --}}
             <div class="row mt-4">
-                <div class="col-md-12">
+                {{-- CHART 1 --}}
+                <div class="col-md-6">
                     <div class="card">
                         <div class="card-header d-flex justify-content-between align-items-center">
                             <h4>Statistik Selesai vs Refund</h4>
@@ -41,18 +28,15 @@
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="content-wrapper mt-4">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h4>Total Transaksi (All Time)</h4>
-                            </div>
-                            <div class="card-body">
-                                <canvas id="donutChart" height="120"></canvas>
-                            </div>
+                {{-- CHART 2 --}}
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Total Transaksi (All Time)</h4>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="donutChart" height="120"></canvas>
                         </div>
                     </div>
                 </div>
