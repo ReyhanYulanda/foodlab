056ca1bc110aff30a8c2909974cfa1b2547cb798
Dukhaan <dukhaank@gmail.com>
2025-09-18 22:11:12 +0700

    change: tenant can now cancel order pesanan_diproses

--- app/Http/Controllers/Transaksi/TransaksiController.php
+++ app/Http/Controllers/Transaksi/TransaksiController.php
@@ -749,10 +749,7 @@ class TransaksiController extends Controller
 
             $isTenant = optional($transaksi->tenant)->user_id == $currentUser->id;
 
-            if (
-                $transaksi->status === 'pesanan_diproses' &&
-                !($currentUser->can('admin cancel order') || $isTenant)
-            ) {
+            if ($transaksi->status === 'pesanan_diproses' && !$currentUser->can('admin cancel order')) {
                 return ResponseApi::error("Pesanan sedang diproses. Tidak bisa dibatalkan", 400);
             }
 
