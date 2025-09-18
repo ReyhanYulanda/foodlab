d4851faa59d8c1d26fb00f7f56abe40ad2d64c8a
Dukhaan <dukhaank@gmail.com>
2025-09-18 22:25:02 +0700

    change: tenant can now cancel order pesanan_diproses

--- app/Http/Controllers/Transaksi/TransaksiController.php
+++ app/Http/Controllers/Transaksi/TransaksiController.php
@@ -747,7 +747,7 @@ class TransaksiController extends Controller
                 return ResponseApi::error("Refund sebelumnya gagal. Silakan hubungi admin", 400);
             }
 
-            $isTenant = $transaksi->tenant_id == optional($currentUser->tenant)->id;
+            $isTenant = $transaksi->tenant_id == $currentUser->id;
 
             if (
                 $transaksi->status === 'pesanan_diproses' &&
