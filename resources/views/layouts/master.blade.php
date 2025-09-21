49213dfd10ee3c8faa7146126f8f3b70a37138aa
Dukhaan <dukhaank@gmail.com>
2025-09-22 02:55:36 +0700

    add: when icon foodlab side bar tapped goes into dashboard

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,8 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
     <title>FoodLab &mdash; PENS</title>
-    <link rel="icon" href="{{ asset('storage/images/logo/logo-foodlab.png') }}" type="image/x-icon">
-    
+    <a href="{{ route('dashboard') }}">
+        <img src="{{ asset('storage/images/logo/logo-foodlab.png') }}" alt="FoodLab Logo" class="h-10 w-auto" />
+    </a>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
         integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
         crossorigin="anonymous" />
@@ -39,8 +41,8 @@
     @stack('css')
     <style>
         .dataTables_wrapper .dataTables_scroll {
-    box-shadow: none !important;
-}
+            box-shadow: none !important;
+        }
     </style>
 
     <!-- Select2 CSS -->
@@ -97,29 +99,27 @@
                 position: 'topRight'
             });
         }
-
-
     </script>
 
     <script>
         @if ($errors->any())
             @foreach ($errors->all() as $error)
-                showMessage('error', "{{$error}}");
+                showMessage('error', "{{ $error }}");
             @endforeach
         @endif
 
-        @if(Session::has('status'))
-            showMessage('success', '{{Session::get("message") ?? ''}}');
+        @if (Session::has('status'))
+            showMessage('success', '{{ Session::get('message') ?? '' }}');
         @endisset
     </script>
 
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener('DOMContentLoaded', function() {
             // Cari semua form dengan method POST dan yang punya _method DELETE
-            document.querySelectorAll('form').forEach(function (form) {
+            document.querySelectorAll('form').forEach(function(form) {
                 const methodInput = form.querySelector('input[name="_method"]');
                 if (methodInput && methodInput.value === 'DELETE') {
-                    form.addEventListener('submit', function (e) {
+                    form.addEventListener('submit', function(e) {
                         const confirmed = confirm('Apakah Anda yakin ingin menghapus data ini?');
                         if (!confirmed) {
                             e.preventDefault(); // Batalkan submit
@@ -129,7 +129,7 @@
             });
         });
     </script>
-    
+
     @stack('js')
 </body>
 
