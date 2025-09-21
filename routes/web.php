d78af4b80df6c5bd39929272811c36d29592360f
Dukhaan <dukhaank@gmail.com>
2025-09-22 01:52:45 +0700

    feat: dashboard statistics

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\EmailVerificationController;
 use App\Http\Controllers\ListAktifDriverController;
 use App\Http\Controllers\NotifikasiController;
+use App\Http\Controllers\Web\DashboardController;
 
 Route::get('/verify-email/{id}/{hash}', [EmailVerificationController::class, 'verify'])
     ->middleware(['signed'])
@@ -45,9 +46,7 @@ Route::middleware(['shared', 'auth', 'role:tenant|kdh|admin'])->group(function (
         return view('pages.welcome.index');
     })->name('welcome');
 
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::resource('menu', MenuController::class);
     Route::resource('role', RoleController::class);
