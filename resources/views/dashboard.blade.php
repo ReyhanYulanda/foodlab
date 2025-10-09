e22858fe94f6d280f75086db2678d5d208a08984
Dukhaan <dukhaank@gmail.com>
2025-10-09 18:48:43 +0700

    add: test cicd bismillah final

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
     @endpush
 
     <div class="main-content">
-        <div class="title">Dashboard CICD SUDAH NYALA BROK</div>
+        <div class="title">Dashboard FIX NO REVISI CICD TERAKHIR</div>
         <div class="content-wrapper">
 
             <div class="row mt-4">
