1aaa1fd366da8f65d544bc1eaab57689b33f504d
Dukhaan <dukhaank@gmail.com>
2025-11-13 20:14:44 +0700

    add: store multitenant with cashbacks voucher

--- app/Http/Controllers/Transaksi/TransaksiController.php
+++ app/Http/Controllers/Transaksi/TransaksiController.php
@@ -558,6 +558,44 @@ class TransaksiController extends Controller
                     }
                 }
 
+                if ($isMultiTenant && !empty($createdTransaksi)) {
+                    $transaksiUntukCashback = $createdTransaksi[0]; // ambil transaksi pertama dalam grup
+
+                    // Cek apakah sudah ada cashback dicatat untuk multitenant ini (jaga-jaga kalau bug double create)
+                    $sudahAdaCashback = CatatVoucher::whereIn(
+                        'transaksi_id',
+                        collect($createdTransaksi)->pluck('id')
+                    )->exists();
+
+                    if (!$sudahAdaCashback) {
+                        // Ambil voucher aktif user (yang valid)
+                        $voucher = Voucher::where('user_id', $user->id)
+                            ->whereHas('cashback', function ($q) {
+                                $q->where('is_valid', true);
+                            })
+                            ->first();
+
+                        if ($voucher) {
+                            CatatVoucher::create([
+                                'user_id' => $user->id,
+                                'transaksi_id' => $transaksiUntukCashback->id,
+                                'voucher_id' => $voucher->id,
+                                'quantity_voucher' => 1,
+                                'cashback_amount' => $voucher->cashback->value,
+                            ]);
+
+                            Log::info('Cashback diberikan hanya ke 1 transaksi multitenant', [
+                                'transaksi_id' => $transaksiUntukCashback->id,
+                                'cashback_value' => $voucher->cashback->value,
+                            ]);
+                        } else {
+                            Log::info('Tidak ada voucher aktif untuk user, cashback dilewati', [
+                                'user_id' => $user->id,
+                            ]);
+                        }
+                    }
+                }
+
                 DB::commit();
 
                 $transaksiWithDetails = Transaksi::with(['listTransaksiDetail.menus'])
