11df401242b421fc180764217a1b7318a63cc38b
Dukhaan <dukhaank@gmail.com>
2025-11-24 20:03:08 +0700

    add: new prevention when multidevice updateStatusPesananCashier

--- app/Services/Kelola/TenantOrderService.php
+++ app/Services/Kelola/TenantOrderService.php
@@ -292,6 +292,42 @@ class TenantOrderService
             return ResponseApi::error('Pesanan kasir sudah selesai sebelumnya.', 403);
         }
 
+        if ($cashier->status === 'selesai' && $request->status === 'pesnan_diproses') {
+            return ResponseApi::error('Pesanan kasir sudah selesai sebelumnya.', 403);
+        }
+
+        if ($cashier->status === 'gagal_bayar' && $request->status === 'selesai') {
+            return ResponseApi::error('Pesanan kasir gagal dibayar.', 403);
+        }
+
+        if ($cashier->status === 'gagal_bayar' && $request->status === 'pesanan_diproses') {
+            return ResponseApi::error('Pesanan kasir gagal dibayar.', 403);
+        }
+
+        if ($cashier->status === 'gagal_bayar' && $request->status === 'pending') {
+            return ResponseApi::error('Pesanan kasir gagal dibayar.', 403);
+        }
+
+        if ($cashier->status === 'pending' && $request->status === 'selesai') {
+            return ResponseApi::error('Pesanan kasir belum dibayar.', 403);
+        }
+
+        if ($cashier->status === 'pending' && $request->status === 'gagal_bayar') {
+            return ResponseApi::error('Pesanan kasir belum dibayar.', 403);
+        }
+
+        if ($cashier->status === 'pending' && $request->status === 'pesanan_diproses') {
+            return ResponseApi::error('Pesanan kasir belum dibayar.', 403);
+        }
+
+        if ($cashier->status === 'pesanan_diproses' && $request->status === 'pending') {
+            return ResponseApi::error('Pesanan kasir sudah dalam proses sebelumnya.', 403);
+        }
+
+        if ($cashier->status === 'pesanan_diproses' && $request->status === 'gagal_bayar') {
+            return ResponseApi::error('Pesanan kasir sudah dalam proses sebelumnya.', 403);
+        }
+
         if ($cashier->status === 'pesanan_diproses' && $request->status === 'pesanan_diproses') {
             return ResponseApi::error('Pesanan kasir sudah dalam proses sebelumnya.', 403);
         }
