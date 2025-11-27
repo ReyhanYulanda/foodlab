20e836753f251336e425867c9cdb2e93d5a05257
Dukhaan <dukhaank@gmail.com>
2025-11-27 23:13:31 +0700

    fix: refund multitenant extra fee

--- app/Http/Controllers/Masbro/PesananController.php
+++ app/Http/Controllers/Masbro/PesananController.php
@@ -680,7 +680,7 @@ class PesananController extends Controller
                         $priorityOngkir = Pengaturan::where('nama', 'ongkos_kirim_prioritas')->value('nilai') ?? 3000;
 
                         // Hitung biaya extra item yang benar
-                        $extraFee = $this->calculateExtraFee($refundTx, $currentTx);
+                        $extraFee = $this->calculateExtraFeeConsistent($refundTx, $currentTx);
 
                         // Hitung refund amount berdasarkan kondisi ongkir
                         if ($refundTx->ongkos_kirim == $ongkirMulti) {
