a5d72abb90f857fc35c06decf7e658a7b93e5b0e
Dukhaan <dukhaank@gmail.com>
2025-12-02 13:43:50 +0700

    fix: driver notification when cancel by web @MonitorTransaksiController

--- app/Http/Controllers/Web/Transaksi/MonitorTransaksiController.php
+++ app/Http/Controllers/Web/Transaksi/MonitorTransaksiController.php
@@ -245,6 +245,52 @@ class MonitorTransaksiController extends Controller
                                 Log::info("ℹ️ Total items ≤ 10, no X to apply");
                             }
                         }
+
+                        if ($transaksi->driver_id == null) {
+                            $masbroTokens = User::role('masbro')
+                                // ->where('isOnline', 1)
+                                ->with('fcmTokens')
+                                ->get()
+                                ->flatMap(fn($user) => $user->fcmTokens->pluck('fcm_token'))
+                                ->filter()
+                                ->unique()
+                                ->values()
+                                ->toArray();
+
+                            $fcmMasbroToken = $masbroTokens;
+                            if (!empty($fcmMasbroToken)) {
+                                $firebases
+                                    ->withNotification('Pesanan prioritas', "Salah satu pesanan prioritas  dibatalkan #{$transaksi->id}")
+                                    ->withData([
+                                        'title' => 'Pesanan Prioritas',
+                                        'body' => "Salah satu pesanan prioritas  dibatalkan #{$transaksi->id}",
+                                        'click_action' => 'FLUTTER_NOTIFICATION_CLICK',
+                                    ])->sendToFallback($fcmMasbroToken);
+                                Log::info('Sending FCM to driver', ['tokens' => $fcmMasbroToken]);
+                            }
+                        } else {
+                            $masbroTokens = User::role('masbro')
+                                ->where('isOnline', 1)
+                                ->with('fcmTokens')
+                                ->get()
+                                ->flatMap(fn($user) => $user->fcmTokens->pluck('fcm_token'))
+                                ->filter()
+                                ->unique()
+                                ->values()
+                                ->toArray();
+
+                            $fcmMasbroToken = $masbroTokens;
+                            if (!empty($fcmMasbroToken)) {
+                                $firebases
+                                    ->withNotification('Pesanan prioritas', "Salah satu pesanan prioritas  dibatalkan #{$transaksi->id}")
+                                    ->withData([
+                                        'title' => 'Pesanan Prioritas',
+                                        'body' => "Salah satu pesanan prioritas  dibatalkan #{$transaksi->id}",
+                                        'click_action' => 'FLUTTER_NOTIFICATION_CLICK',
+                                    ])->sendToDriver($fcmMasbroToken);
+                                Log::info('Sending FCM to driver', ['tokens' => $fcmMasbroToken]);
+                            }
+                        }
                     } else {
                         Log::info("ℹ️ Tidak ada transaksi aktif lain dalam multitenant #{$transaksi->multitenant_id}");
                     }
