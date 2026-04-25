a069d4424db3aed746ba77c09b0f9abf284d37d7
Dukhaan <dukhaank@gmail.com>
2026-04-25 16:21:34 +0700

    testing: actions workflow

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
         <x-slot name="logo">
             <a href="/">
                 <h1 class="mb-12 text-center text-4xl font-thin">
-                    MasBro Canteen cek git actions 16:13
+                    MasBro Canteen cek git actions 16:21
                 </h1>
                 {{-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
             </a>
